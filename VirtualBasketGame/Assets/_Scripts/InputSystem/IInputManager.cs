0cd8c9f738d38dbd27eed6979ee25d82ab8e4013
JuanELG <42153902+JuanELG@users.noreply.github.com>
2025-03-14 17:01:33 -0500

    Implement input manager

---
+++ VirtualBasketGame/Assets/_Scripts/InputSystem/IInputManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+public interface IInputManager
+{
+    event Action AimStarted;
+    event Action AimCanceled;
+    Vector2 AimVector { get; }
+}
---
+++ VirtualBasketGame/Assets/_Scripts/InputSystem/InputManager.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour, IInputManager
+{
+    public event Action AimStarted;
+    public event Action AimCanceled;
+
+    private GameControls gameControl = null;
+
+    public Vector2 AimVector => gameControl.Gameplay.Aim.ReadValue<Vector2>();
+
+    private void Awake()
+    {
+        gameControl = new GameControls();
+    }
+
+    private void OnEnable()
+    {
+        gameControl.Enable();
+        gameControl.Gameplay.Aim.started += RaiseAimStarted;
+        gameControl.Gameplay.Aim.canceled += RaiseAimCanceled;
+    }
+
+    private void OnDisable()
+    {
+        gameControl.Disable();
+        gameControl.Gameplay.Aim.started -= RaiseAimStarted;
+        gameControl.Gameplay.Aim.canceled -= RaiseAimCanceled;
+    }
+
+    private void RaiseAimStarted(InputAction.CallbackContext context)
+    {
+        AimStarted?.Invoke();
+    }
+
+    private void RaiseAimCanceled(InputAction.CallbackContext context)
+    {
+        AimCanceled?.Invoke();
+    }
+}
